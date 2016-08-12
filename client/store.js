93ccd9b8b389d12abcdf12d4a7cbe5c4b8c6cfeb
Justin Ramel <justin.ramel@ncl.ac.uk>
2016-08-12 11:31:14 +0100

    initial commit

---
+++ client/store.js
@@ -0,0 +1,9 @@
+import {createStore, compse} from 'redux'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {browserHistory} from 'react-router'
+
+import rootReducer from './reducers/index'
+
+
+import comments from './data/comments'
+import posts from './data/posts'
